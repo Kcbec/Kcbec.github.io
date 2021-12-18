205d2ef1c0ba33fcc40143d9824c2456712be8e1
Kcbec <70987844+Kcbec@users.noreply.github.com>
2021-12-17 22:58:08 -0500

    I uh...forgot to push the JS file. Whoops

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,29 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+//comment section functions
+$(document).ready(function(){ 
+     
+    $(".primaryContained").on('click', function(){
+    $(".comment").addClass("commentClicked");
+  });//end click
+  $("textarea").on('keyup.enter', function(){
+    $(".comment").addClass("commentClicked");
+  });//end keyup
+  });//End Function
+ 
+new Vue({
+    el: "#app",
+    data:{
+       title: 'Add a comment',
+      newItem: '',
+      item: [],
+    },
+    methods:{
+      addItem  (){
+      this.item.push(this.newItem);
+        this.newItem = "";
+      }
+  }
+ 
+  });
